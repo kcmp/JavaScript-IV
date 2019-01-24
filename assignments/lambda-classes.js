ff33522bc8e2ff994f9286d83603c5f585296ed0
unknown <kcmp711@gmail.com>
2019-01-24 12:35:16 -0500

    added old code to prototype-refactor.js and completed lambda-classes.js without stretch goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,68 @@
 // CODE here for your Lambda Classes
+
+class People {
+    constructor (attributes) {
+       this.name = attributes.name;
+       this.age = attributes.age;
+       this.location = attributes.location;
+       this.gender = attributes.gender;
+    }
+    speak (){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends People {  // child of People 
+    constructor (instructor) {
+        super (instructor)
+        this.favLanguage = instructor.favLanguage;
+        this.specialty = instructor.specialty;
+        this.catchPhrase = instructor.catchPhrase;
+    }
+    demo (subject){
+        return `Today we are learning about ${subject}.`
+    }
+    grade (student, subject){
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
+
+class Student extends People {  // child of People 
+    constructor (theStudent) {
+        super (theStudent)
+        this.previousBackground = theStudent.previousBackground;
+        this.className = theStudent.className;
+        this.favSubjects = theStudent.favSubjects;
+    } 
+    listSubjects (){
+        this.favSubjects.forEach((subject)=> {
+            console.log(subject)
+        });
+    }
+    PRassignment (subject){
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge (subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+
+}
+
+class ProjectManager extends Instructor{ // extends Instructor
+    constructor (pm) {
+        super (pm)
+        this.gradClassName = pm.gradClassName;
+        this.favInstructor = pm.favInstructor;
+    }
+    standUp (channel){
+       return  `${this.name} announces to ${channel}, @channel standby times!`
+    }
+    debugsCode (student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
+// stretch goals version 
+// CODE here for your Lambda Classes
+
+
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,45 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+
+//GameObject
+// function GameObject(object) {
+//     this.createdAt = object.createdAt;
+//     this.dimensions = object.dimensions;
+//   }
+  
+//   GameObject.prototype.destroy = function() {
+//     return `${this.name} was removed from the game.`;
+//   };
+
+//CharacterStats
+// function CharacterStats(gameStats) {
+//     GameObject.call(this, gameStats);
+//     this.healthPoints = gameStats.healthPoints;
+//     this.name = gameStats.name;
+  
+//   }
+  
+//   CharacterStats.prototype = Object.create(GameObject.prototype);
+  
+//   CharacterStats.prototype.takeDamage = function() {
+//     return `${this.name} took damage.`;
+  
+//   };
+
+//Humaniod
+// function Humanoid (gameHumanoid) {
+//     this.team = gameHumanoid.team;
+//     this.weapons = gameHumanoid.weapons;
+//     this.language = gameHumanoid.language;
+  
+//     CharacterStats.call(this, gameHumanoid);
+//   }
+  
+//   Humanoid.prototype = Object.create(CharacterStats.prototype);
+  
+//   Humanoid.prototype.greet = function() {
+//     return `${this.name} offers a greeting in ${this.language}`;
+//   }
+  
